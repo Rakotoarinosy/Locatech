5d70b025c07c17bd726d37d86dd116e9d4871624
Rakotoarinosy <fehizoroarinosy@gmail.com>
2026-06-15 15:07:20 +0300

    fix: local static files and environment configuration

--- locatech/backend/urls.py
+++ locatech/backend/urls.py
@@ -12,3 +12,14 @@ urlpatterns = [
     path('factures/', include('apps.factures.urls')),
     path('analytics/', include('apps.analytics.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
